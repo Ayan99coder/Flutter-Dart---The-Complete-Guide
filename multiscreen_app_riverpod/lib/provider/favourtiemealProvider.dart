e88f20cf64a0f054fa165cedef6e293cf51fddca
Ayan javed <ayanjaved803@gmail.com>
2026-07-27 11:18:52 +0500

    yes

--- multiscreen_app_riverpod/lib/provider/favourtiemealProvider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:multiscreen_app_riverpod/model/meals_model.dart';
-import 'package:multiscreen_app_riverpod/viewmodel/favourtie_notfier.dart';
-
-final favMealProvider = NotifierProvider<FavouriteNotifier,List<MealsModel>>(FavouriteNotifier.new);
--- multiscreen_app_riverpod/lib/provider/meals_screen_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:multiscreen_app_riverpod/data/dummmy_data.dart';
-
-final mealsProvider = Provider((ref){return meals;});
